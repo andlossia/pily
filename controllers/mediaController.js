12db837e9c52c2389377211fb44dcff65e01b4a9
Andlossia <69584141+andlossia@users.noreply.github.com>
2025-01-12 09:07:28 +0200

    Update mediaController.js

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,6 +1,6 @@
 const initController = require('./genericController');
 const Media = require('../models/mediaModel');
 
-const mediaController = initController(Media, "Media", {}, [], []);
+const mediaController = initController(Media, "Media", {}, [], ['']);
 
 module.exports = mediaController;
